efb44dfc8b572c88055afc813faaa7e4f7c2fde7
Damjan15 <stamenovdamjan20@gmail.com>
2022-12-30 09:47:33 +0100

    tailwindcss set up, project setup

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        darkCyan: "#0E8784",
+        darkGreyBlue: "#333D4B",
+        paleOrange: "#FDD6BA",
+        lightCream: "#FEFCF7",
+        grey: "#83888F"
+      }
+    },
+  },
+  plugins: [],
+}
